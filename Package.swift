032315f29b66ad4930e26dd6f07281abb9c0c43d
jasonikim <56625575+jasonikim@users.noreply.github.com>
2020-04-07 13:08:33 -0700

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "Nuke-WebP-Plugin",
+    platforms: [
+        .macOS(.v10_13),
+        .iOS(.v11),
+        .tvOS(.v11),
+        .watchOS(.v4)
+    ],
+    products: [
+        .library(name: "Nuke-WebP-Plugin", targets: ["Nuke-WebP-Plugin"])
+    ],
+    targets: [
+        .target(name: "Nuke-WebP-Plugin", path: "Sources")
+    ]
+)
